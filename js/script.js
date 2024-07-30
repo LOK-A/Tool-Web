3436188a168fea6c30e895dec7b1f0e912502800
LOK4V1D4 <heronpessoa_@hotmail.com>
2024-07-30 11:46:44 -0300

    vagv

--- js/script.js
+++ js/script.js
@@ -1,305 +1,293 @@
-let espStub;
+const butClear = document.getElementById("botLimpar");
+const baudrates = 921600;
+const consoleBaudrates = document.getElementById("baudRate");
+const connectButton = document.getElementById("botConectar");
+const resetButton = document.getElementById("botReset");
+const eraseButton = document.getElementById("botApagar");
+const consoleStartButton = document.getElementById("botStartTerminal");
+const programButton = document.getElementById("botUpload");
+const terminal = document.getElementById("monitor");
+const logo = document.getElementById("logo");
+const addFileButton = document.getElementById("addFile");
+const variacoes = [":)", ";)"];
+const chipID = document.getElementById("chipID");
 
-const baudRates = [9600, 115200, 230400, 460800, 921600];
+const table = document.getElementById("fileTable");
 
-const bufferSize = 512;
-const colors = ["#00a7e9", "#f89521", "#be1e2d"];
-const measurementPeriodId = "0001";
+declare let Terminal; // Terminal is imported in HTML script
+declare let CryptoJS; // CryptoJS is imported in HTML script
 
-const maxLogLength = 100;
-const log = document.getElementById("monitor");
-const butConnect = document.getElementById("butConnect");
-const baudRate = document.getElementById("baudRate");
-const butClear = document.getElementById("butClear");
-const butErase = document.getElementById("butErase");
-const butReset = document.getElementById("butReset")
-const butProgram = document.getElementById("butUpload");
-const autoscroll = document.getElementById("butScroll");
-const firmware = document.querySelectorAll(".upload .firmware input");
-const progress = document.querySelectorAll(".upload .progress-bar");
-const offsets = document.querySelectorAll(".upload .offset");
+const term = new Terminal({ cols: 159, rows: 41 });
+term.open(terminal);
 
-document.addEventListener("DOMContentLoaded", () => {
-    butConnect.addEventListener("click", () => {
-        clickConnect().catch(async (e) => {
-            console.error(e);
-            errorMsg(e.message || e);
-            if (espStub) {
-                await espStub.disconnect();
-            }
-            toggleUIConnected(false);
-        });
-    });
-    butClear.addEventListener("click", clickClear);
-    butReset.addEventListener("click", clickReset);
-    butErase.addEventListener("click", clickErase);
-    butProgram.addEventListener("click", clickProgram);
-    autoscroll.addEventListener("click", clickAutoscroll);
-    baudRate.addEventListener("change", changeBaudRate);
-    window.addEventListener("error", function (event) {
-        console.log("Ocorreu um erro não detectado: ", event.error);
-    });
-
-    initBaudRate();
-    loadAllSettings();
-});
-
-function initBaudRate() {
-    for (let rate of baudRates) {
-        var option = document.createElement("option");
-        option.text = rate;
-        option.value = rate;
-        baudRate.add(option);
-    }
-}
-
-function logMsg(text) {
-    log.innerHTML += text + "<br>";
-
-    // Remove old log content
-    if (log.textContent.split("\n").length > maxLogLength + 1) {
-        let logLines = log.innerHTML.replace(/(\n)/gm, "").split("<br>");
-        log.innerHTML = logLines.splice(-maxLogLength).join("<br>\n");
-    }
-
-    if (scroll) {
-        log.scrollTop = log.scrollHeight;
-    }
-}
-
-
-function errorMsg(text) {
-    logMsg('<span class="error-message">Error:</span> ' + text);
-    console.error(text);
-}
-
-
-function formatMacAddr(macAddr) {
-    return macAddr
-        .map((value) => value.toString(16).toUpperCase().padStart(2, "0"))
-        .join(":");
-}
+let device = null;
+let transport: Transport;
+let chip: string = null;
+let esploader: ESPLoader;
+let indiceVariacoes = 0;
 
 /**
- * @name clickConnect
- * Click handler for the connect/disconnect button.
+ * The built in Event object.
+ * @external Event
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Event}
  */
-async function clickConnect() {
-    logMsg("");
-    logMsg("");
 
-    if (espStub) {
-        await espStub.disconnect();
-        await espStub.port.close();
-        toggleUIConnected(false);
-        espStub = undefined;
+/**
+ * File reader handler to read given local file.
+ * @param {Event} evt File Select event
+ */
+function handleFileSelect(evt) {
+    const file = evt.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (ev: ProgressEvent<FileReader>) => {
+        evt.target.data = ev.target.result;
+    };
+
+    reader.readAsBinaryString(file);
+}
+
+const espLoaderTerminal = {
+    clean() {
+        term.clear();
+    },
+    writeLine(data) {
+        term.writeln(data);
+    },
+    write(data) {
+        term.write(data);
+    },
+};
+
+connectButton.onclick = async () => {
+    if (conectadoUI) {
+        if (transport) await transport.disconnect();
+        cleanUp();
+    }
+
+    else {
+        if (device === null) {
+            device = await navigator.serial.requestPort({});
+            transport = new Transport(device, true);
+        }
+
+        try {
+            const flashOptions = {
+                transport,
+                baudrate: parseInt(baudrates.value),
+                terminal: espLoaderTerminal,
+            } as LoaderOptions;
+            esploader = new ESPLoader(flashOptions);
+
+            chip = await esploader.main();
+
+            conectadoUI(true);
+        } catch (e) {
+            errorMsg(e);
+            conectadoUI(false);
+        }
+
+        console.log("Configurações finalizadas no " + chip);
+        chipID.innerHTML = "Conectado a " + chip;
+    }
+};
+
+resetButton.onclick = async () => {
+    if (transport) {
+        await transport.setDTR(false);
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        await transport.setDTR(true);
+    }
+};
+
+eraseButton.onclick = async () => {
+    eraseButton.disabled = true;
+    try {
+        await esploader.eraseFlash();
+    } catch (e) {
+        errorMsg(e);
+    } finally {
+        eraseButton.disabled = false;
+    }
+};
+
+addFileButton.onclick = () => {
+    const rowCount = table.rows.length;
+    const row = table.insertRow(rowCount);
+
+    //Column 1 - Offset
+    const cell1 = row.insertCell(0);
+    const element1 = document.createElement("input");
+    element1.type = "text";
+    element1.id = "offset" + rowCount;
+    element1.value = "0x1000";
+    cell1.appendChild(element1);
+
+    // Column 2 - File selector
+    const cell2 = row.insertCell(1);
+    const element2 = document.createElement("input");
+    element2.type = "file";
+    element2.id = "selectFile" + rowCount;
+    element2.name = "selected_File" + rowCount;
+    element2.addEventListener("change", handleFileSelect, false);
+    cell2.appendChild(element2);
+
+    // Column 3  - Progress
+    const cell3 = row.insertCell(2);
+    cell3.classList.add("progress-cell");
+    cell3.innerHTML = `<progress value="0" max="100"></progress>`;
+
+    // Column 4  - Remove File
+    const cell4 = row.insertCell(3);
+    cell4.classList.add("action-cell");
+    if (rowCount > 1) {
+        const element4 = document.createElement("input");
+        element4.type = "button";
+        const btnName = "button" + rowCount;
+        element4.name = btnName;
+        element4.setAttribute("class", "btn");
+        element4.setAttribute("value", "Remove"); // or element1.value = "button";
+        element4.onclick = function () {
+        };
+        cell4.appendChild(element4);
+    }
+};
+
+
+/**
+ * Clean devices variables on chip disconnect. Remove stale references if any.
+ */
+function cleanUp() {
+    device = null;
+    transport = null;
+    chip = null;
+}
+
+
+let isConsoleClosed = false;
+consoleStartButton.onclick = async () => {
+    if (device === null) {
+        device = await navigator.serial.requestPort({});
+        transport = new Transport(device, true);
+    }
+
+    await transport.connect(parseInt(consoleBaudrates.value));
+    isConsoleClosed = false;
+
+    while (true && !isConsoleClosed) {
+        const val = await transport.rawRead();
+        if (typeof val !== "undefined") {
+            term.write(val);
+        } else {
+            break;
+        }
+    }
+    console.log("quitting console");
+};
+
+/**
+ * Validate the provided files images and offset to see if they're valid.
+ * @returns {string} Program input validation result
+ */
+function validateProgramInputs() {
+    const offsetArr = [];
+    const rowCount = table.rows.length;
+    let row;
+    let offset = 0;
+    let fileData = null;
+
+    // check for mandatory fields
+    for (let index = 1; index < rowCount; index++) {
+        row = table.rows[index];
+
+        //offset fields checks
+        const offSetObj = row.cells[0].childNodes[0];
+        offset = parseInt(offSetObj.value);
+
+        // Non-numeric or blank offset
+        if (Number.isNaN(offset)) return "Offset field in row " + index + " is not a valid address!";
+        // Repeated offset used
+        else if (offsetArr.includes(offset)) return "Offset field in row " + index + " is already in use!";
+        else offsetArr.push(offset);
+
+        const fileObj = row.cells[1].childNodes[0];
+        fileData = fileObj.data;
+        if (fileData == null) return "No file selected for row " + index + "!";
+    }
+    return "success";
+}
+
+programButton.onclick = async () => {
+    const alertMsg = document.getElementById("alertmsg");
+    const err = validateProgramInputs();
+
+    if (err != "success") {
+        alertMsg.innerHTML = "<strong>" + err + "</strong>";
         return;
     }
 
-    const esploaderMod = await window.esptoolPackage;
+    const fileArray = [];
+    const progressBars = [];
 
-    const esploader = await esploaderMod.connect({
-        log: (...args) => logMsg(...args),
-        error: (...args) => errorMsg(...args),
-    });
-    try {
-        await esploader.initialize();
+    for (let index = 1; index < table.rows.length; index++) {
+        const row = table.rows[index];
 
-        logMsg("Connected to " + esploader.chipName);
-        logMsg("MAC Address: " + formatMacAddr(esploader.macAddr()));
+        const offSetObj = row.cells[0].childNodes[0];
+        const offset = parseInt(offSetObj.value);
 
-        espStub = await esploader.runStub();
-        toggleUIConnected(true);
-        espStub.addEventListener("disconnect", () => {
-            toggleUIConnected(false);
-            espStub = false;
-        });
-    } catch (err) {
-        await esploader.disconnect();
-        throw err;
+        const fileObj = row.cells[1].childNodes[0] as ChildNode & { data: string };
+        const progressBar = row.cells[2].childNodes[0];
+
+        progressBar.textContent = "0";
+        progressBars.push(progressBar);
+
+        fileArray.push({ data: fileObj.data, address: offset });
     }
-}
 
-/**
- * @name changeBaudRate
- * Change handler for the Baud Rate selector.
- */
-async function changeBaudRate() {
-    saveSetting("baudrate", baudRate.value);
-    if (espStub) {
-        let baud = parseInt(baudRate.value);
-        if (baudRates.includes(baud)) {
-            await espStub.setBaudrate(baud);
+    try {
+        const flashOptions: FlashOptions = {
+            fileArray: fileArray,
+            flashSize: "keep",
+            eraseAll: false,
+            compress: true,
+            reportProgress: (fileIndex, written, total) => {
+                progressBars[fileIndex].value = (written / total) * 100;
+            },
+            calculateMD5Hash: (image) => CryptoJS.MD5(CryptoJS.enc.Latin1.parse(image)),
+        } as FlashOptions;
+        await esploader.writeFlash(flashOptions);
+    } catch (e) {
+        errorMsg(e);
+    } finally {
+        // Hide progress bars and show erase buttons
+        for (let index = 1; index < table.rows.length; index++) {
         }
     }
+};
+
+addFileButton.onclick(this);
+
+butClear.onclick = async () => {
+    term.reset();
+};
+
+function errorMsg(text) {
+    term.writeln('<span class="error-message">Erro:</span> ' + text);
+    console.error(text);
 }
 
-/**
- * @name clickAutoscroll
- * Change handler for the Autoscroll checkbox.
- */
-async function clickAutoscroll() {
-
-    if (scroll) {
-        scroll = false;
-    }
-    else {
-        scroll = true;
-    }
-}
-
-
-/**
- * @name clickErase
- * Click handler for the erase button.
- */
-async function clickErase() {
-    if (
-        window.confirm("Isso apagará toda a flash. Clique em OK para continuar.")
-    ) {
-        try {
-            logMsg("Apagando memoria flash. Aguarde...");
-            let stamp = Date.now();
-            await espStub.eraseFlash();
-            logMsg("Finalizado. Demorou " + (Date.now() - stamp) + "ms para apagar.");
-        } catch (e) {
-            errorMsg(e);
-        }
-    }
-}
-
-/**
- * @name clickProgram
- * Click handler for the program button.
- */
-async function clickProgram() {
-    let bootloader = import("./bin/bootloader.bin");
-    let partitions = import("./bin/partitions.bin");
-    let app = import("./bin/app.bin");
-    let firm = import("./bin/firmware.bin");
-
-    try {
-        await espStub.flashData(
-            bootloader,
-            (bytesWritten, totalBytes) => {
-                progressBar.style.width =
-                    Math.floor((bytesWritten / totalBytes) * 100) + "%";
-            },
-            4096
-        );
-        await sleep(100);
-    } catch (e) {
-        errorMsg(e);
-    }
-
-    try {
-        await espStub.flashData(
-            partitions,
-            (bytesWritten, totalBytes) => {
-                progressBar.style.width =
-                    Math.floor((bytesWritten / totalBytes) * 100) + "%";
-            },
-            32768
-        );
-        await sleep(100);
-    } catch (e) {
-        errorMsg(e);
-    }
-
-    try {
-        await espStub.flashData(
-            app,
-            (bytesWritten, totalBytes) => {
-                progressBar.style.width =
-                    Math.floor((bytesWritten / totalBytes) * 100) + "%";
-            },
-            57344
-        );
-        await sleep(100);
-    } catch (e) {
-        errorMsg(e);
-    }
-
-    try {
-        await espStub.flashData(
-            firm,
-            (bytesWritten, totalBytes) => {
-                progressBar.style.width =
-                    Math.floor((bytesWritten / totalBytes) * 100) + "%";
-            },
-            65536
-        );
-        await sleep(100);
-    } catch (e) {
-        errorMsg(e);
-    }
-
-    logMsg("Para executar o novo firmware, reinicie o seu dispositivo.");
-}
-
-/**
- * @name clickClear
- * Click handler for the clear button.
- */
-async function clickClear() {
-    // reset();     Reset function wasnt declared.
-    log.innerHTML = "";
-}
-
-function convertJSON(chunk) {
-    try {
-        let jsonObj = JSON.parse(chunk);
-        return jsonObj;
-    } catch (e) {
-        return chunk;
-    }
-}
-
-function toggleUIConnected(connected) {
+function conectadoUI(connected) {
     let lbl = "Conectar ESP";
     if (connected) {
         lbl = "Desconectar ESP";
     }
-    butConnect.textContent = lbl;
+    botConectar.innerHTML = lbl;
 }
 
-function loadAllSettings() {
-    autoscroll.checked = loadSetting("autoscroll", true);
-    baudRate.value = loadSetting("baudrate", 115200);
-}
-
-function loadSetting(setting, defaultValue) {
-    let value = JSON.parse(window.localStorage.getItem(setting));
-    if (value == null) {
-        return defaultValue;
-    }
-
-    return value;
-}
-
-function saveSetting(setting, value) {
-    window.localStorage.setItem(setting, JSON.stringify(value));
-}
-
-function ucWords(text) {
-    return text
-        .replace("_", " ")
-        .toLowerCase()
-        .replace(/(?<= )[^\s]|^./g, (a) => a.toUpperCase());
-}
-
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function clickReset() {
-    try {
-        await espStub.hardReset();
-        logMsg("");
-        logMsg("");
-
-    } catch (e) {
-        errorMsg(e);
-    }
-}
-
+setInterval(() => {
+    logo.innerHTML = variacoes[indiceVariacoes];
+    indiceVariacoes = (indiceVariacoes + 1) % variacoes.length;
+}, 1000);
